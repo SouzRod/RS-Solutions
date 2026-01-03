782e1b3b7a158bc000065bccaa418481bf7da39a
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2026-01-03 00:56:26 -0300

    quebrando test de proposito

--- test/application/useCase/add-user.spec.ts
+++ test/application/useCase/add-user.spec.ts
@@ -24,7 +24,7 @@ describe("AddUserImpl", () => {
       birthDate: "2001-01-01T00:00:00.000Z",
     };
 
-    await addUser.execute(userInput);
+    await addUser.execute({});
 
     expect(userRepositoryMock.insertOne).toHaveBeenCalledTimes(1);
   });
